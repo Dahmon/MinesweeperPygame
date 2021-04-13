266f12ced3aa4fbe9a39383f6ac63ab9ae7431e6
dahmon.bicheno <dahmon.bicheno@gmail.com>
2021-04-13 23:09:23 +1000

    * Refactored self.cells into multidimension array to better handle revealing cells * Moved win condition check to handle win helper function, renamed _checkWinCondition

--- minesweeper.py
+++ minesweeper.py
@@ -74,23 +74,27 @@ class Minesweeper:
 		self.sprites.add(self.face)
 
 	def _initGame(self):
-		# self.board = Board(boardWidth, boardHeight)
-		for y in range(self.boardHeight):
-			for x in range(self.boardWidth):
-				cell = Cell(x * 16, (y * 16) + self.faceButtonRowHeight)
+		for row in range(self.boardHeight):
+			self.cells.append([])
+			for col in range(self.boardWidth):
+				cell = Cell(col * 16, (row * 16) + self.faceButtonRowHeight)
 				self.sprites.add(cell)
-				self.cells.append(cell)
+				self.cells[row].append(cell)
 		
 		self._generateBombs(self.bombCount)
 
 	def _generateBombs(self, bombCount):
 		bombs = 0
 		while bombs < bombCount:
-			cell = self.cells[random.randint(0, len(self.cells) - 1)]
+			randomRow = random.randint(0, self.boardHeight - 1)
+			randomCol = random.randint(0, self.boardWidth - 1)
+
+			cell = self.cells[randomRow][randomCol]
 			if not cell.isBomb:
 				cell.isBomb = True
 				bombs += 1
 				cell.applySprite(cell.bomb if self.showBombs else cell.normal)
+
 
 	def _updateScreen(self):
 		# fill the background with white
@@ -116,12 +120,13 @@ class Minesweeper:
 		self.clock.tick(30)
 
 	def _resetGame(self):
-		for cell in self.cells:
-			cell.applySprite(cell.normal)
-			cell.isActive = True
-			cell.isBomb = False
-			cell.isPressed = False
-			cell.lockedState = 0
+		for row in self.cells:
+			for cell in row:
+				cell.applySprite(cell.normal)
+				cell.isActive = True
+				cell.isBomb = False
+				cell.isPressed = False
+				cell.lockedState = 0
 
 		self.face.applySprite(self.face.smile)
 		self.flagDisplay.setDisplay(self.bombCount)
@@ -141,9 +146,10 @@ class Minesweeper:
 
 			if event.type == KEYDOWN and event.key == K_b:
 				self.showBombs = not self.showBombs
-				for cell in self.cells:
-					if cell.isBomb:
-						cell.applySprite(cell.bomb if self.showBombs else cell.normal)
+				for row in self.cells:
+					for cell in row:
+						if cell.isBomb:
+							cell.applySprite(cell.bomb if self.showBombs else cell.normal)
 
 			# Holding both mouse buttons should clear area
 
@@ -167,128 +173,107 @@ class Minesweeper:
 
 	def _handleCellMouseDown(self, event):
 		flaggedCells = 0
-		for cell in self.cells:
-			if cell.isActive and cell.rect.collidepoint(pygame.mouse.get_pos()):
-				# if left clicking, handle cell opening
-				if event.button == 1 and not cell.lockedState:
-					cell.isPressed = True
-					cell.applySprite(cell.clicked)
+		for row in self.cells:
+			for cell in row:
+				if cell.isActive and cell.rect.collidepoint(pygame.mouse.get_pos()):
+					# if left clicking, handle cell opening
+					if event.button == 1 and not cell.lockedState:
+						cell.isPressed = True
+						cell.applySprite(cell.clicked)
 
-				# if right clicking, handle cell flagging and question marking
-				if event.button == 3 and cell.isActive:
-					if cell.lockedState < 2:
-						cell.lockedState += 1
-						cell.applySprite(cell.flag if cell.lockedState == 1 else cell.question)
-					else:
-						cell.lockedState = 0
-						cell.applySprite(cell.normal)
+					# if right clicking, handle cell flagging and question marking
+					if event.button == 3 and cell.isActive:
+						if cell.lockedState < 2:
+							cell.lockedState += 1
+							cell.applySprite(cell.flag if cell.lockedState == 1 else cell.question)
+						else:
+							cell.lockedState = 0
+							cell.applySprite(cell.normal)
 			
-			if cell.lockedState:
-				flaggedCells += 1
+				if cell.lockedState:
+					flaggedCells += 1
 
 		self.flagDisplay.setDisplay(self.bombCount - flaggedCells)
-		print(self.clock.get_time())
 
 	def _handleCellMouseUp(self, event):
-		for cell in self.cells:
-			if cell.isActive:
-				if cell.isPressed and cell.rect.collidepoint(pygame.mouse.get_pos()):
-					self.clickCount += 1
+		for row in self.cells:
+			for cell in row:
+				if cell.isActive:
+					if cell.isPressed and cell.rect.collidepoint(pygame.mouse.get_pos()):
+						self.clickCount += 1
 
-					if cell.isBomb:
-						if self.clickCount == 1:
-							cell.isBomb = False
-							self._generateBombs(1)
-							self._checkCellNeighbours(self.cells.index(cell))
+						if cell.isBomb:
+							if self.clickCount == 1:
+								cell.isBomb = False
+								self._generateBombs(1)
+								self._checkCellNeighbours(self.cells.index(row), row.index(cell))
+							else:
+								self._handleBombClick()
 						else:
-							self._handleBombClick()
-					else:
-						self._checkCellNeighbours(self.cells.index(cell))
-				elif cell.isPressed:
-					cell.applySprite(cell.normal)
+							self._checkCellNeighbours(self.cells.index(row), row.index(cell))
+					elif cell.isPressed:
+						cell.applySprite(cell.normal)
 
-				cell.isPressed = False
+					cell.isPressed = False
 
 
-	def _checkCellNeighbours(self, cellIndex):
-		neighbouringCellIndexes = [
-			(cellIndex - self.boardWidth) - 1, (cellIndex - self.boardWidth), (cellIndex - self.boardWidth) + 1,
-			cellIndex - 1, cellIndex + 1,
-			(cellIndex + self.boardWidth) - 1, (cellIndex + self.boardWidth), (cellIndex + self.boardWidth) + 1,
-		]
-		neighbouringBombs = 0
-
-		# get neighbouringBombs count
-		# replace with list reduce?
-		for neighbourIndex in neighbouringCellIndexes:
-			if self._isInBounds(neighbourIndex):
-				neighbourCell = self.cells[neighbourIndex]
-
-				if neighbourCell.isBomb:
-					neighbouringBombs += 1
-
-		cell = self.cells[cellIndex]
+	def _checkCellNeighbours(self, row, col):
+		cell = self.cells[row][col]
 		cell.isActive = False
 		self.revealedCellCount += 1
+		neighbouringBombs = 0
 
-		expectedRevealedCount = math.floor((self.boardWidth * self.boardHeight) - self.bombCount)
-		if self.revealedCellCount == expectedRevealedCount:
-			self._handleWin()
+		# Check win condition
+		self._checkWinCondition()
 
-		# I THINK BREAKING BECAUSE NEIGHBOR INDEX WRAPS WHEN NEGATIVE OR MORE THAN CELL LIST LENGTH
-		# change sprite and call function on neightbours if required
+		for neighbourRow in (row - 1, row, row + 1):
+			if neighbourRow >= 0 and neighbourRow <= (self.boardHeight - 1):
+				for neighbourCol in (col - 1, col, col + 1):
+					if neighbourCol >= 0 and neighbourCol <= (self.boardWidth - 1):
+						if self.cells[neighbourRow][neighbourCol].isBomb:
+							neighbouringBombs += 1
+
 		if neighbouringBombs == 0:
 			cell.applySprite(cell.clicked)
-			for neighbourIndex in neighbouringCellIndexes:
-				if self._isInBounds(neighbourIndex):
-					neighbourCell = self.cells[neighbourIndex]
-
-					if neighbourCell.isActive:
-						self._checkCellNeighbours(neighbourIndex)
+			for neighbourRow in (row - 1, row, row + 1):
+				if neighbourRow >= 0 and neighbourRow <= (self.boardHeight - 1):
+					for neighbourCol in (col - 1, col, col + 1):
+						if neighbourCol >= 0 and neighbourCol <= (self.boardWidth - 1):
+							neighbourCell = self.cells[neighbourRow][neighbourCol]
+							if neighbourCell.isActive:
+								self._checkCellNeighbours(neighbourRow, neighbourCol)
+							neighbourCell.isActive = False
 		else:
-			# Apply number sprite
 			cell.applySprite(cell.numberSprites[neighbouringBombs - 1])
-
-
-	def _isInBounds(self, cellIndex):
-		# ignore out of range indexes
-		if cellIndex >= 0 and cellIndex < len(self.cells):
-			return True
-			# rowFirstIndex = (math.floor(cellIndex / self.boardWidth) * self.boardWidth)
-			# rowLastIndex = rowFirstIndex + self.boardWidth - 1
-			# outOfBoundIndexes = [
-			# 	(rowFirstIndex - self.boardWidth) - 1, (rowLastIndex - self.boardWidth) + 1,
-			# 	rowFirstIndex - 1, rowLastIndex + 1,
-			# 	(rowFirstIndex + self.boardWidth) - 1, (rowLastIndex + self.boardWidth) + 1,
-			# ]
-
-			# return cellIndex not in outOfBoundIndexes
-		
-		return False
 
 	def _handleBombClick(self):
 		self.face.applySprite(self.face.dead)
 		self.isRunning = False
 
-		for cell in self.cells:
-			cell.isActive = False
-			if cell.isBomb:
-				if not cell.lockedState == 1:
-					if cell.rect.collidepoint(pygame.mouse.get_pos()):
-						cell.applySprite(cell.bombClicked)
-					else:
-						cell.applySprite(cell.bomb)
-			elif cell.lockedState:
-				cell.applySprite(cell.bombIncorrect)
+		for row in self.cells:
+			for cell in row:
+				cell.isActive = False
 
-	def _handleWin(self):
-		self.face.applySprite(self.face.win)
-		self.isRunning = False
+				if cell.isBomb:
+					if not cell.lockedState == 1:
+						if cell.rect.collidepoint(pygame.mouse.get_pos()):
+							cell.applySprite(cell.bombClicked)
+						else:
+							cell.applySprite(cell.bomb)
+				elif cell.lockedState:
+					cell.applySprite(cell.bombIncorrect)
 
-		for cell in self.cells:
-			cell.isActive = False
-			if cell.isBomb:
-				cell.applySprite(cell.flag)
+	def _checkWinCondition(self):
+		expectedRevealedCount = math.floor((self.boardWidth * self.boardHeight) - self.bombCount)
+		if self.revealedCellCount == expectedRevealedCount:
+			self.face.applySprite(self.face.win)
+			self.isRunning = False
+
+			for row in self.cells:
+				for cell in row:
+					cell.isActive = False
+					if cell.isBomb:
+						cell.applySprite(cell.flag)
 
 if __name__ == '__main__':
 	minesweeper = Minesweeper()
